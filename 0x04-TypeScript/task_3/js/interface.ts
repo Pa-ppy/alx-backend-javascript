29657369276df972043b2a25f1bc69716f94d34b
Pa-ppy <rayjacks07@gmail.com>
2025-04-24 05:12:52 +0000

    Task 3

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Define a type alias for row ID
+export type RowID = number;
+
+// Define the interface for row elements
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
