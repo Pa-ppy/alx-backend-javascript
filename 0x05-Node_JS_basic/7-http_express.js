35017af54bdece650ccf3c12ea9dad415834b4d4
Pa-ppy <rayjacks07@gmail.com>
2025-06-10 14:10:56 +0000

    Task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+      const fields = {};
+
+      for (const line of students) {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+
+      const output = [];
+      output.push(`Number of students: ${students.length}`);
+      for (const field in fields) {
+        output.push(
+          `Number of students in ${field}: ${
+            fields[field].length
+          }. List: ${fields[field].join(', ')}`,
+        );
+      }
+      resolve(output.join('\n'));
+    });
+  });
+}
+
+const database = process.argv[2];
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello ALX!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const data = await countStudents(database);
+    res.type('text/plain');
+    res.send(`This is the list of our students\n${data}`);
+  } catch (err) {
+    res.type('text/plain');
+    res.send('This is the list of our students\nCannot load the database');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
