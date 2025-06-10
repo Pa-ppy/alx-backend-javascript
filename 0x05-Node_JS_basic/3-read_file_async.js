3c1fca41f968276a4c97d4ab5e852377a1a4add9
Pa-ppy <rayjacks07@gmail.com>
2025-06-10 14:31:07 +0000

    Task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,33 +2,33 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) return reject(new Error('Cannot load the database'));
 
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1);
+      const lines = data.trim().split('\n');
+      lines.shift();
+      const result = {};
 
-      const fields = {};
-      for (const line of students) {
-        const [firstname, , , field] = line.split(',');
-        if (!fields[field]) {
-          fields[field] = [];
+      for (const line of lines) {
+        if (line) {
+          const [first, , , field] = line.split(',');
+          if (!result[field]) result[field] = [];
+          result[field].push(first);
         }
-        fields[field].push(firstname);
       }
 
-      console.log(`Number of students: ${students.length}`);
-      for (const field in fields) {
-        console.log(
-          `Number of students in ${field}: ${
-            fields[field].length
-          }. List: ${fields[field].join(', ')}`,
-        );
+      console.log(`Number of students: ${lines.length}`);
+      for (const field in result) {
+        if (Object.prototype.hasOwnProperty.call(result, field)) {
+          console.log(
+            `Number of students in ${field}: ${
+              result[field].length
+            }. List: ${result[field].join(', ')}`,
+          );
+        }
       }
-      resolve();
+
+      return resolve();
     });
   });
 }
