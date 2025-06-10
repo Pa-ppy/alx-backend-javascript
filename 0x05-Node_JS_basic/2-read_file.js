0c2b0c10f84feed693ba64b3eb16847ae5419b25
Pa-ppy <rayjacks07@gmail.com>
2025-06-10 14:08:57 +0000

    Task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1); // Skip header
+
+    const fields = {};
+    for (const line of students) {
+      const [firstname, , , field] = line.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    }
+
+    console.log(`Number of students: ${students.length}`);
+    for (const field in fields) {
+      console.log(
+        `Number of students in ${field}: ${
+          fields[field].length
+        }. List: ${fields[field].join(', ')}`,
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
