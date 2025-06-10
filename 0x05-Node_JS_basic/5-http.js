e889029e03f4ec3c5e4fb4c2db8cf213ae5ac90f
Pa-ppy <rayjacks07@gmail.com>
2025-06-10 14:31:21 +0000

    Task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,61 +1,46 @@
 const http = require('http');
 const fs = require('fs');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
+const app = http.createServer((req, res) => {
+  const { url } = req;
+
+  if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello ALX!');
+  } else if (url === '/students') {
+    const dbPath = process.argv[2];
+    fs.readFile(dbPath, 'utf8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Cannot load the database');
         return;
       }
 
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1);
-      const fields = {};
+      const lines = data.trim().split('\n').slice(1).filter(Boolean);
+      const result = {};
 
-      for (const line of students) {
-        const [firstname, , , field] = line.split(',');
-        if (!fields[field]) {
-          fields[field] = [];
+      for (const line of lines) {
+        const [first, , , field] = line.split(',');
+        if (!result[field]) result[field] = [];
+        result[field].push(first);
+      }
+
+      let response = 'This is the list of our students\n';
+      response += `Number of students: ${lines.length}\n`;
+
+      for (const field in result) {
+        if (Object.prototype.hasOwnProperty.call(result, field)) {
+          response += `Number of students in ${field}: ${
+            result[field].length
+          }. List: ${result[field].join(', ')}\n`;
         }
-        fields[field].push(firstname);
       }
 
-      const output = [];
-      output.push(`Number of students: ${students.length}`);
-      for (const field in fields) {
-        output.push(
-          `Number of students in ${field}: ${
-            fields[field].length
-          }. List: ${fields[field].join(', ')}`,
-        );
-      }
-      resolve(output.join('\n'));
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(response.trim());
     });
-  });
-}
-
-const database = process.argv[2];
-
-const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello ALX!');
-  } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    countStudents(database)
-      .then((data) => {
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch(() => {
-        res.end('This is the list of our students\nCannot load the database');
-      });
-  } else {
-    res.writeHead(404);
-    res.end();
   }
 });
 
 app.listen(1245);
-
 module.exports = app;
