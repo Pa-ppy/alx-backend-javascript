b3bc0a5bc28ae7038f26634115ff7261bdba6477
Pa-ppy <rayjacks07@gmail.com>
2025-06-12 21:34:24 +0000

    Task 4

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,18 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should stub Utils.calculateNumber and spy console.log', () => {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const logSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledWith(stub, 'SUM', 100, 20);
+    sinon.assert.calledWith(logSpy, 'The total is: 10');
+
+    stub.restore();
+    logSpy.restore();
+  });
+});
